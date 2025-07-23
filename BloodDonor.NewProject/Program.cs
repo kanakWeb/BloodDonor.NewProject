18ea0a799ae83e72bc0da922ebda0a055551243d
kanakWeb <kanakwebdev@gmail.com>
2025-07-23 23:11:56 +0600

    added some solve

--- BloodDonor.NewProject/Program.cs
+++ BloodDonor.NewProject/Program.cs
@@ -11,6 +11,19 @@ builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<BloodDonorDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+<<<<<<< Updated upstream
+=======
+
+builder.Services.AddScoped<IBloodDonorRepository, BloodDonorRepository>();
+builder.Services.AddScoped<IBloodDonorService, BloodDonorService>();
+builder.Services.AddTransient<IFileService, FileService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IDonationRepository, DonationRepository>();
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+
+
+>>>>>>> Stashed changes
 var app = builder.Build();
 
 
---
+++ BloodDonor.NewProject/Repositories/Implementations/BloodDonorRepository.cs
@@ -0,0 +1,16 @@
+﻿using BloodDonor.Models;
+using BloodDonor.NewProject.Data;
+using BloodDonor.NewProject.Models.Entities;
+using BloodDonor.NewProject.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BloodDonor.NewProject.Repositories.Implementations
+{
+    public class BloodDonorRepository : Repository<BloodDonorEntity>, IBloodDonorRepository
+    {
+        public BloodDonorRepository(BloodDonorDbContext context) : base(context)
+        {
+        }
+    }
+}
